f11c7842a10650d886445dd987070d6043f4f911
Marco Guevara <marco.algv@gmail.com>
2024-03-28 09:38:10 -0600

    Initial Commit

---
+++ DragonBallApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragonBallApp
+//
+//  Created by Marco Guevara on 28/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragonBallApp/DragonBallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallAppApp.swift
+//  DragonBallApp
+//
+//  Created by Marco Guevara on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
